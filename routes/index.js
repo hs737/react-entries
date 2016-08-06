2ca8c0f9bc564c7fbe3cb9e37c024d9f30d68f1f
Harsh <hsinha146@gmail.com>
2016-08-06 09:31:37 -0400

    Completed isomorphic mirroring on both server and client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var react = require('react')
+var reactRouter = require('react-router')
+var reactDomServer = require('react-dom/server')
+
+var ServerApp = require('../public/build/es5/ServerApp')
+var Main = require('../public/build/es5/components/Main')
+
 var router = express.Router();
+require('node-jsx').install({extension: '.js'})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    html = reactDomServer.renderToString(react.createElement(ServerApp))
+    res.render('index', {
+        title: 'Express',
+        react: html
+    });
 });
 
 module.exports = router;
